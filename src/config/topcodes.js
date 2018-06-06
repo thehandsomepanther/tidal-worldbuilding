b1dd0663c676f6aee71d919f81c7f36fd2b67045
Josh Shi <thehandsomepanther@gmail.com>
2018-06-06 15:38:33 -0500

    update codes

--- src/config/topcodes.js
+++ src/config/topcodes.js
@@ -1,10 +1,11 @@
 export const topcodeFeatures = {
-  47: "water",
-  31: "water",
-  55: "water",
-  61: "forest",
-  117: "forest",
-  203: "forest",
-  79: "forest",
-  103: "forest"
+  171: "water",
+  167: "water",
+  107: "water",
+  55: "forest",
+  157: "forest",
+  31: "forest",
+  109: "mountain",
+  103: "mountain",
+  47: "mountain"
 };
